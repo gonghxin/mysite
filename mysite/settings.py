775e209070507bc617b44b30d2edef17ded75985
gonghxin <gonghxin@gmail.com>
2016-05-20 17:47:44 +0800

    databse

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,16 +75,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+ #     'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'book',
+    #    'USER':'root',
+     #   'PASSWORD':'admin',
+#        'HOST':'localhost',
+ #       'PORT':'3306',
+  #  }
+#}
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'book',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':'3306',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
